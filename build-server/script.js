2b7742a72bccea2238ab68398f8d868a22b1c72c
Harsh Sharma <Harshsharma1421@gmail.com>
2024-02-19 23:51:07 +0530

    added-comments

--- build-server/script.js
+++ build-server/script.js
@@ -1,9 +1,24 @@
 import {exec} from 'child_process'
 import path from 'path'
+import fs from 'fs'
 
 async function init(){
     console.log("Executing Script.js")
     const outDirPath = path.join(__dirname, 'output')
 
     const p = exex(`cd ${outDirPath} && npm install && npm run build`)
+    p.stdout.on('data', (data)=>{
+        console.log(data.toString())
+    })
+
+    p.stdout.on('error', (data)=>{
+        console.log('Error',data.toString())
+    })
+
+    p.on('close', async()=>{
+        console.log(`Build Complete`)
+        // After build is sucessfull build folder is created which has html or css or .. files
+        const distFolderPath = path.join(__dirname, 'output', 'dist')
+        const distFolderContent = fs.readdirSync(distFolderPath , {recursive: true}) // recursive because it read all folder (data insdide folder) and it gives an array
+    })
 }
